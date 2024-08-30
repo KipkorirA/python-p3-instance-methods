9e56370b3ca02886e7e23d82723842e585cc5cdb
KipkorirA <aron.kipkorir@student.moringaschool.com>
2024-08-30 21:56:41 +0300

    Done

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+    def sit(self):
+        print("The dog is sitting.")
+    
+    def bark(self):
+        print("Woof!")
 
-    #Instance method definition
-    pass
+if __name__ == "__main__":
+    my_dog = Dog()
+    my_dog.sit()  # This will print: The dog is sitting.
+    my_dog.bark() # This will print: Woof!
